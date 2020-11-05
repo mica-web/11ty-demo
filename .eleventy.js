ec19e62ca7921cdfbbfedb0d2b21b4ca20dc730e
Glitch (rhinestone-even-soccer) <none>
2020-11-05 15:28:40 +0000

    🚌👧 Checkpoint ./src/index.html:9869298/2 ./src/_includes/card.html:9869298/4 ./.eleventy.js:9869298/1080

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
 // docs: https://www.11ty.io/docs/config/
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addLayoutAlias("default", "default.html");
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.setTemplateFormats(["css", "html", "liquid"]);
 
   function sortByPageOrder(values) {
-      return values.slice().sort((a, b) => a.data.order - b.data.order);
+    return values.slice().sort((a, b) => a.data.order - b.data.order);
   }
   eleventyConfig.addFilter("sortByPageOrder", sortByPageOrder);
 
